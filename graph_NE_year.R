49ca91b9f56f4521463c8fe07e5e7318cee6a55c
Zo Kupelian <zkupelian@umass.edu>
2026-06-24 16:35:28 -0400

    Baseline year significance in New England and interpretation

--- graph_NE_year.R
+++ graph_NE_year.R
@@ -1,4 +1,10 @@
-graph_NE_year <- function(year) {
+graph_NE_year <- function(data, year) {
+  data_ne <- subset(
+    data,
+    longitude >= -73.7 & longitude <= -66.9 &  #these coords include all of
+      latitude  >= 41.0  & latitude  <= 47.5     #the NE states, can be expanded
+  )
+  
   ggplot() +
     geom_polygon(
       data = ne_states,
@@ -7,7 +13,7 @@ graph_NE_year <- function(year) {
       color = "white"
     ) +
     geom_point(
-      data = FET %>%
+      data = data_ne %>%
         filter(collection_year == year),
       aes(x = longitude, y = latitude, color = factor(tick_infection)),
       alpha = 0.5,
